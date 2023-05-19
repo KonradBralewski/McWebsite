de9e3812cf4f4ec0279656d489a7196d88d01553
ShleeRe <konradbralewski1111@gmail.com>
2023-05-19 19:11:58 +0200

    Added auth. middleware (JWT Bearer)

--- src/McWebsite.Infrastructure/DependencyInjection.cs
+++ src/McWebsite.Infrastructure/DependencyInjection.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Configuration;
 using McWebsite.Application.Common.Interfaces.Authentication;
 using McWebsite.Application.Common.Interfaces.Persistence;
 using McWebsite.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Options;
 
 namespace McWebsite.Infrastructure
 {
@@ -13,11 +17,35 @@ namespace McWebsite.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
         {
-            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+            services.AddAuth(configuration);
 
             services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddAuth(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            JwtSettings jwtSettings = new JwtSettings();
+            configuration.Bind(JwtSettings.SectionName, jwtSettings);
+
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+            services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                });
 
             return services;
         }
